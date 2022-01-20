a950e6751cb8df3939c2a133edc683eefaba9ecb
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-01-20 12:27:16 -0300

    Alterado "src/app.service.ts": agora é possível buscar individualmente por um filme, através da chave "_id" gerada automaticamente ao armazenar um objeto no mongoDb.

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,19 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   home(): string {
     return (
-      '<a href="http://localhost:3000/movies">List all movies</a>'
+      `
+        <div>
+          <a href="http://localhost:3000/movies">
+            List all movies
+          </a>
+          <br><br>
+          <form action="" method="link" onsubmit="this.action='http://localhost:3000/movies/'+this.q.value;return true;">
+            <label for="q">Search movie by mongoDb Object "_id"</label><br>
+            <input type="text" id="q" name="q">
+            <input type="submit" value="Search">
+          </form>
+        </div>
+      `
     );
   }
 }
